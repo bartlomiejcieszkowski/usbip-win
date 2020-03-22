72f8a97491f7c7ea28e88916c9ef15f4f18e213d
Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2020-03-22 17:27:21 +0100

    correct dbgprint with mask

--- driver/lib/dbgcommon.h
+++ driver/lib/dbgcommon.h
@@ -9,9 +9,9 @@
 
 #include "usbip_proto.h"
 
-#define DBGE(part, fmt, ...)	DbgPrintEx(MY_DFPLTR, DPFLTR_MASK | (part), DRVPREFIX ":(EE) " fmt, ## __VA_ARGS__)
-#define DBGW(part, fmt, ...)	DbgPrintEx(MY_DFPLTR, DPFLTR_MASK | ((part) << 1), DRVPREFIX ":(WW) " fmt, ## __VA_ARGS__)
-#define DBGI(part, fmt, ...)	DbgPrintEx(MY_DFPLTR, DPFLTR_MASK | ((part) << 2), DRVPREFIX ": " fmt, ## __VA_ARGS__)
+#define DBGE(part, fmt, ...)	DbgPrintEx(MY_DFPLTR, DPFLTR_MASK | (part << 4) | DPFLTR_ERROR_LEVEL, DRVPREFIX ":(EE) " fmt, ## __VA_ARGS__)
+#define DBGW(part, fmt, ...)	DbgPrintEx(MY_DFPLTR, DPFLTR_MASK | (part << 4) | DPFLTR_WARNING_LEVEL, DRVPREFIX ":(WW) " fmt, ## __VA_ARGS__)
+#define DBGI(part, fmt, ...)	DbgPrintEx(MY_DFPLTR, DPFLTR_MASK | (part << 4) | DPFLTR_INFO_LEVEL, DRVPREFIX ": " fmt, ## __VA_ARGS__)
 
 int dbg_snprintf(char *buf, int size, const char *fmt, ...);
 
