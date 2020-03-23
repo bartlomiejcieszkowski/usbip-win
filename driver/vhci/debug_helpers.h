7baa794a2fb43781a2bef0f8e8b596c9e03d04bd
Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2020-03-23 19:35:47 +0100

    debug helpers for IRQL issues

--- driver/vhci/debug_helpers.h
+++ driver/vhci/debug_helpers.h
@@ -10,8 +10,22 @@
 #define DBGI_FUN(part, fmt, ...) \
     DBGI(part, __FILE__ "@" TO_STR(__LINE__) "" fmt, ## __VA_ARGS__)
 
+#define LOG_IRQL_NE(expected_irql) \
+    do { \
+        KIRQL test_irql = KeGetCurrentIrql(); \
+        if (expected_irql != test_irql) { \
+            DBGI_FUN(DBG_GENERAL, "IRQL: %u (expected %u)", KeGetCurrentIrql(), expected_irql); \
+        } \
+    } while (0)
+
 #undef IoCompleteRequest
 #define IoCompleteRequest(a,b) \
     DBGI_FUN(DBG_GENERAL, "IoCompleteRequest %p boost? %u", a, b); \
+    LOG_IRQL_NE(PASSIVE_LEVEL); \
     IofCompleteRequest(a, b)
-     
+
+#undef IoCallDriver
+#define IoCallDriver(a,b)   \
+    DBGI_FUN(DBG_GENERAL, "IoCallDriver devobj:%p irp:%p", a, b); \
+    LOG_IRQL_NE(PASSIVE_LEVEL); \
+    IofCallDriver(a,b)
