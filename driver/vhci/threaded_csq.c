cfbce4c76115fe9cde26dd0a33d15dc61a2d2edc
Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2020-03-29 14:18:01 +0200

    rename and fixup

--- driver/vhci/threaded_csq.c
+++ driver/vhci/threaded_csq.c
@@ -1,49 +1,48 @@
-#include "csq_with_thread.h"
+#include "threaded_csq.h"
 
-VOID csq_with_thread_complete_cancelled_irp(_In_ PIO_CSQ csq, _In_ PIRP irp)
+VOID _threaded_csq_complete_cancelled_irp(_In_ PIO_CSQ csq, _In_ PIRP irp)
 {
+	UNREFERENCED_PARAMETER(csq);
 	irp->IoStatus.Status = STATUS_CANCELLED;
 	irp->IoStatus.Information = 0;
 
 	IoCompleteRequest(irp, IO_NO_INCREMENT);
 }
 
-_Acquires_lock_(CONTAINING_RECORD(csq, csq_with_thread, irp_csq)->irp_csq_lock)
-VOID csq_with_thread_acquire_lock(_In_ PIO_CSQ csq, _Out_ PKIRQL irql)
+_Acquires_lock_(CONTAINING_RECORD(csq, threaded_csq, irp_csq)->irp_csq_lock)
+VOID _threaded_csq_acquire_lock(_In_ PIO_CSQ csq, _Out_ PKIRQL irql)
 {
-	pcsq_with_thread ctx = CONTAINING_RECORD(csq, csq_with_thread, irp_csq);
+	pthreaded_csq ctx = CONTAINING_RECORD(csq, threaded_csq, irp_csq);
 	KeAcquireSpinLock(&ctx->irp_csq_lock, irql);
 }
 
-_Releases_lock_(CONTAINING_RECORD(csq, csq_with_thread, irp_csq)->irp_csq_lock)
-VOID csq_with_thread_release_lock(_In_ PIO_CSQ csq, _In_ KIRQL irql)
+_Releases_lock_(CONTAINING_RECORD(csq, threaded_csq, irp_csq)->irp_csq_lock)
+VOID _threaded_csq_release_lock(_In_ PIO_CSQ csq, _In_ KIRQL irql)
 {
-	pcsq_with_thread ctx = CONTAINING_RECORD(csq, csq_with_thread, irp_csq);
+	pthreaded_csq ctx = CONTAINING_RECORD(csq, threaded_csq, irp_csq);
 	KeReleaseSpinLock(&ctx->irp_csq_lock, irql);
 }
 
-VOID csq_with_thread_insert_irp(_In_ PIO_CSQ csq, _In_ PIRP irp)
+VOID _threaded_csq_insert_irp(_In_ PIO_CSQ csq, _In_ PIRP irp)
 {
-	pcsq_with_thread ctx = CONTAINING_RECORD(csq, csq_with_thread, irp_csq);
+	pthreaded_csq ctx = CONTAINING_RECORD(csq, threaded_csq, irp_csq);
 	InsertTailList(&ctx->irp_csq_list, &irp->Tail.Overlay.ListEntry);
 }
 
-VOID csq_with_thread_remove_irp(_In_ PIO_CSQ csq, _In_ PIRP irp)
+VOID _threaded_csq_remove_irp(_In_ PIO_CSQ csq, _In_ PIRP irp)
 {
 	UNREFERENCED_PARAMETER(csq);
 	RemoveEntryList(&irp->Tail.Overlay.ListEntry);
 }
 
-PIRP csq_with_thread_peek_next_irp(PIO_CSQ csq, PIRP irp, PVOID peek_context)
+PIRP _threaded_csq_peek_next_irp(PIO_CSQ csq, PIRP irp, PVOID peek_context)
 {
 	UNREFERENCED_PARAMETER(peek_context);
-	pcsq_with_thread ctx = CONTAINING_RECORD(csq, csq_with_thread, irp_csq);
+	pthreaded_csq ctx = CONTAINING_RECORD(csq, threaded_csq, irp_csq);
 	PLIST_ENTRY next_entry = NULL;
-	PIRP next_irp = NULL;
-	PIO_STACK_LOCATION irp_stack = NULL;
 
 	if (irp == NULL) {
-		next_entry = &ctx->irp_csq_list.Flink;
+		next_entry = ctx->irp_csq_list.Flink;
 	}
 	else {
 		next_entry = irp->Tail.Overlay.ListEntry.Flink;
@@ -55,11 +54,10 @@ PIRP csq_with_thread_peek_next_irp(PIO_CSQ csq, PIRP irp, PVOID peek_context)
 		: NULL;
 }
 
-VOID csq_with_thread_main(_In_ PVOID context)
+VOID threaded_csq_main(_In_ PVOID context)
 {
-	pcsq_with_thread ctx = (pcsq_with_thread)context;
+	pthreaded_csq ctx = (pthreaded_csq)context;
 	PIRP irp;
-	NTSTATUS status;
 
 	KeSetPriorityThread(KeGetCurrentThread(), LOW_REALTIME_PRIORITY);
 
--- driver/vhci/threaded_csq.h
+++ driver/vhci/threaded_csq.h
@@ -6,7 +6,7 @@ typedef VOID CSQ_WITH_THREAD_PROCESS_IRP(_In_ PVOID context, _In_ PIRP irp);
 typedef CSQ_WITH_THREAD_PROCESS_IRP * PCSQ_WITH_THREAD_PROCESS_IRP;
 
 // this structures abstracts csq and thread so this pattern can be reused if needed elsewhere
-typedef struct _csq_with_thread {
+typedef struct _threaded_csq {
 	PETHREAD thread;
 	// Something to wait on, so we avoid busy-wait in thread
 	KSEMAPHORE thread_semaphore;
@@ -19,17 +19,17 @@ typedef struct _csq_with_thread {
 
 	PCSQ_WITH_THREAD_PROCESS_IRP process_irp_fn;
 	PVOID process_irp_ctx;
-} csq_with_thread, *pcsq_with_thread;
+} threaded_csq, *pthreaded_csq;
 
-IO_CSQ_COMPLETE_CANCELED_IRP csq_with_thread_complete_cancelled_irp;
-IO_CSQ_ACQUIRE_LOCK csq_with_thread_acquire_lock;
-IO_CSQ_RELEASE_LOCK csq_with_thread_release_lock;
-IO_CSQ_INSERT_IRP csq_with_thread_insert_irp;
-IO_CSQ_REMOVE_IRP csq_with_thread_remove_irp;
-IO_CSQ_PEEK_NEXT_IRP csq_with_thread_peek_next_irp;
-VOID csq_with_thread_main(_In_ PVOID context);
+IO_CSQ_COMPLETE_CANCELED_IRP _threaded_csq_complete_cancelled_irp;
+IO_CSQ_ACQUIRE_LOCK _threaded_csq_acquire_lock;
+IO_CSQ_RELEASE_LOCK _threaded_csq_release_lock;
+IO_CSQ_INSERT_IRP _threaded_csq_insert_irp;
+IO_CSQ_REMOVE_IRP _threaded_csq_remove_irp;
+IO_CSQ_PEEK_NEXT_IRP _threaded_csq_peek_next_irp;
+VOID threaded_csq_main(_In_ PVOID context);
 
-inline NTSTATUS csq_with_thread_init(_In_ pcsq_with_thread ctx,
+inline NTSTATUS threaded_csq_init(_In_ pthreaded_csq ctx,
 	_In_ PCSQ_WITH_THREAD_PROCESS_IRP process_irp_fn,
 	_In_ PVOID process_irp_ctx)
 {
@@ -37,12 +37,12 @@ inline NTSTATUS csq_with_thread_init(_In_ pcsq_with_thread ctx,
 	KeInitializeSpinLock(&ctx->irp_csq_lock);
 	KeInitializeSemaphore(&ctx->thread_semaphore, 0, MAXLONG);
 	IoCsqInitialize(&ctx->irp_csq,
-		csq_with_thread_insert_irp,
-		csq_with_thread_remove_irp,
-		csq_with_thread_peek_next_irp,
-		csq_with_thread_acquire_lock,
-		csq_with_thread_release_lock,
-		csq_with_thread_complete_cancelled_irp);
+		_threaded_csq_insert_irp,
+		_threaded_csq_remove_irp,
+		_threaded_csq_peek_next_irp,
+		_threaded_csq_acquire_lock,
+		_threaded_csq_release_lock,
+		_threaded_csq_complete_cancelled_irp);
 
 	ctx->thread_stop = FALSE;
 	ctx->process_irp_fn = process_irp_fn;
@@ -59,7 +59,7 @@ inline NTSTATUS csq_with_thread_init(_In_ pcsq_with_thread ctx,
 		NULL,
 		(HANDLE)0,
 		NULL,
-		csq_with_thread_main,
+		threaded_csq_main,
 		ctx);
 
 	if (!NT_SUCCESS(status))
@@ -79,8 +79,27 @@ inline NTSTATUS csq_with_thread_init(_In_ pcsq_with_thread ctx,
 	return status;
 }
 
-inline void csq_with_thread_insert_irp(_In_ pcsq_with_thread ctx, _In_ PIRP irp)
+inline VOID threaded_csq_cleanup(_In_ pthreaded_csq ctx)
+{
+	ctx->thread_stop = TRUE;
+	KeReleaseSemaphore(&ctx->thread_semaphore, 0, 1, TRUE);
+	KeWaitForSingleObject(ctx->thread, Executive, KernelMode, FALSE, NULL);
+	ObDereferenceObject(ctx->thread);
+
+	PIRP irp = IoCsqRemoveNextIrp(&ctx->irp_csq, NULL);
+	while (irp)
+	{
+		irp->IoStatus.Information = 0;
+		irp->IoStatus.Status = STATUS_CANCELLED;
+		IoCompleteRequest(irp, IO_NO_INCREMENT);
+		irp = IoCsqRemoveNextIrp(&ctx->irp_csq, NULL);
+	}
+}
+
+inline VOID threaded_csq_insert_irp(_In_ pthreaded_csq ctx, _In_ PIRP irp)
 {
 	IoCsqInsertIrp(&ctx->irp_csq, irp, NULL);
 	KeReleaseSemaphore(&ctx->thread_semaphore, 0, 1, FALSE);
 }
+
+#define USING_CSQ_WITH_THREAD 1
